57b1e8685d0ffb0097c35d2e644d0ca158f68cea
cogjava1675 <sanchitsingh468@gmail.com>
2022-09-08 11:05:34 +0530

    code changes for body and path

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -19,6 +21,14 @@ public class DemoApplication {
 	public String  message(){
 		return "Hi there !";
 	}
-
-
+	@GetMapping("/messagepath/{message}")
+	public String messagePath(@PathVariable String message){
+		String messagePath = "Hi there ! The message you typed was " + message;
+		return messagePath;
+	}
+	@GetMapping("/messagebody")
+	public String messageBody(@RequestBody String message){
+		String messageBody = "Hi there ! The message you typed in the body was " + message;
+		return messageBody;
+	}
 }
